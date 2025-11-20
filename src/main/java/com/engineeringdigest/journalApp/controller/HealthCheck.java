c1f7fee4a4b3f347a61bc8fd6d06997d05fcac00
Vaishnavi <vaishnavisubash09@gmail.com>
2025-11-21 00:58:13 +0530

    updated health-check with new message

--- src/main/java/com/engineeringdigest/journalApp/controller/HealthCheck.java
+++ src/main/java/com/engineeringdigest/journalApp/controller/HealthCheck.java
@@ -8,6 +8,7 @@ public class HealthCheck {
 
     @GetMapping("/health-check")
     public String healthCheck(){
-        return "hey dear user.... You might be crying out tears of joy after see that your app is running in the host!!\nYou are making progress";
+        return "hey dear user.... You might be crying out tears of joy after see that your app is running in the host!!You are making progress";
     }
 }
+
